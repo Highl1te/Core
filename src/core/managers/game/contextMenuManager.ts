16a264f2695df05d773e3931cb81215503e27393
KKonaOG <kkonaog@fanet.dev>
2025-05-20 22:16:00 -0500

    Context Menu Manager was referencing old locations

--- src/core/managers/game/contextMenuManager.ts
+++ src/core/managers/game/contextMenuManager.ts
@@ -1,5 +1,5 @@
-import { ActionState } from "../interfaces/game/actionStates.enum";
-import { ContextMenuTypes } from "../interfaces/game/contextMenuTypes.enum";
+import { ActionState } from "../../interfaces/game/actionStates.enum";
+import { ContextMenuTypes } from "../../interfaces/game/contextMenuTypes.enum";
 
 export enum EntityType {
     Any = -1,
