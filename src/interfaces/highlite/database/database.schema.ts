deb310d49fb78780a992834ffc78390331f59b53
Riley Lane <rileylanea@gmail.com>
2025-08-23 12:22:11 -0700

    Remove OBE drop log entry in Schema

--- src/interfaces/highlite/database/database.schema.ts
+++ src/interfaces/highlite/database/database.schema.ts
@@ -39,22 +39,6 @@ export interface HighliteSchema extends DBSchema {
         key: string; // User Name
         value: Record<string, Record<string, boolean | number | string>>; // Plugin Name -> Setting Name -> Value
     };
-    drop_logs: {
-        key: number; //NPC defId
-        value: {
-            defId: number;
-            name: string;
-            killCount: number;
-            drops: {
-                [itemId: number]: {
-                    name: string;
-                    quantity: number;
-                    totalDropped: number;
-                };
-            };
-            lastUpdated: number;
-        };
-    };
     data: {
         key: string; // User Name
         value: {
